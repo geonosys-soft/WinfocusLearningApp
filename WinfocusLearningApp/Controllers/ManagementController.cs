991d95fd76647c29256457ed52af6dba1634fd7a
Subin-code <102280544+Subin444@users.noreply.github.com>
2025-07-21 09:06:25 +0530

    from subin

--- WinfocusLearningApp/Controllers/ManagementController.cs
+++ WinfocusLearningApp/Controllers/ManagementController.cs
@@ -15,7 +15,11 @@ namespace WinfocusLearningApp.Controllers
             return RedirectToAction("AllUsers");
         }
 
-        public ActionResult AllUsers() { return View(); }
+        public ActionResult AllUsers() { 
+        //code updated
+            return View();
+        }
+
         public ActionResult CreateUsers() { return View(); }
         public ActionResult AcademicYear() { return View(); }
         public ActionResult ClassManagement() { return View(); }
