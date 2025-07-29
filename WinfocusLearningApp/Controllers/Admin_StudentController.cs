373565c8482c28f865c33e2a90c744fdfade12a2
Subin-code <102280544+Subin444@users.noreply.github.com>
2025-07-30 01:46:20 +0530

    colour
    colour and registration list ,view
    colour enhance

---
+++ WinfocusLearningApp/Controllers/Admin_StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WinfocusLearningApp.Controllers
+{
+    public class Admin_StudentController : Controller
+    {
+        // GET: Admin_Student
+        public ActionResult RegistrationList()
+        {
+            return View();
+        }
+        public ActionResult RegistrationListview()
+        {
+            return View();
+        }
+    }
+}
