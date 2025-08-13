da33fc137f861719d263e8b646a90a80138811e4
azharkiz <36560281+azharkiz@users.noreply.github.com>
2025-08-13 13:34:57 +0530

    update

--- WinfocusLearningApp/Controllers/DTPController.cs
+++ WinfocusLearningApp/Controllers/DTPController.cs
@@ -1,24 +1,81 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WinfocusLearningApp.DataEntity;
 
 namespace WinfocusLearningApp.Controllers
 {
     public class DTPController : Controller
     {
+        Winfocus_CS db=new Winfocus_CS();
+
         // GET: DTP
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult Dashboard()
         {
             return View();
         }
-        public ActionResult QuestionAnswers() { 
-        return View();
+
+        public ActionResult QuestionAnswers(int? Id)
+        {
+            TblQuestionAnswer answer = new TblQuestionAnswer();
+            if (Id == null)
+            {
+              
+                ViewBag.AccademicYearId = new SelectList(db.TblAccademicYears.Where(p => p.IsDeleted == 0), "Id", "AccademicYear");
+                ViewBag.SyllabusID = new SelectList(Enumerable.Empty<string>());
+                ViewBag.ClassID = new SelectList(Enumerable.Empty<string>());
+                ViewBag.StreamID = new SelectList(Enumerable.Empty<string>());
+                ViewBag.SubjectID = new SelectList(Enumerable.Empty<string>());
+                ViewBag.ChapterID = new SelectList(Enumerable.Empty<string>());
+                ViewBag.QuestionType = new SelectList(new[]
+                    {
+                        new { Id = 1, Name = "MCQ" },
+                        new { Id = 2, Name = "Teacher MCQ" },
+                        new { Id = 3, Name = "Question Bank" }
+                    }, "Id", "Name");
+                ViewBag.Week=new SelectList(new[] 
+                { 
+                    new { Id = 1, Name="Week 1" },
+                    new { Id = 2, Name="Week 2" },
+                    new { Id = 3, Name="Week 3" },
+                    new { Id = 4, Name="Week 4" },
+                }, "Id","Name");
+                ViewBag.Month = new SelectList(new[]
+                {
+                    new { Id = 1, Name="January" },
+                    new { Id = 2, Name="February" },
+                    new { Id = 3, Name="March" },
+                    new { Id = 4, Name="April" },
+                    new { Id = 5, Name="May" },
+                    new { Id = 6, Name="June" },
+                    new { Id = 7, Name="July" },
+                    new { Id = 8, Name="August" },
+                    new { Id = 9, Name="September" },
+                    new { Id = 10,Name="October" },
+                    new { Id = 11,Name="November" },
+                    new { Id = 12,Name="December" },
+                }, "Id", "Name");
+                
+                ViewBag.Answer = new SelectList(new[]
+                {
+                    new { Id = 1, Name="Option 1" },
+                    new { Id = 2, Name="Option 2" },
+                    new { Id = 3, Name="Option 3" },
+                    new { Id = 4, Name="Option 4" },
+                }, "Id", "Name");
+            }
+            else
+            {
+            }
+            return View();
         }
     }
 }
---
+++ WinfocusLearningApp/WebApi/QAController.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WinfocusLearningApp.DataEntity;
+
+namespace WinfocusLearningApp.WebApi
+{
+    public class QAController : ApiController
+    {
+        [Route("api/QA/AddQuestion")]
+        [HttpPost]
+        public IHttpActionResult AddQuestion()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+
+                // 1. Get the uploaded file
+                var file = httpRequest.Files["file"];
+                if (file != null && file.ContentLength > 0)
+                {
+                   // string filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + file.FileName);
+                   // file.SaveAs(filePath);
+                }
+
+                // 2. Get the JSON data from form-data
+                var jsonData = httpRequest.Form["jsonData"];
+                if (string.IsNullOrEmpty(jsonData))
+                {
+                    return BadRequest("No JSON data received.");
+                }
+
+                var questionData = JsonConvert.DeserializeObject<TblQuestionAnswer>(jsonData);
+
+                // 3. Save questionData to database
+                // db.TblQuestionAnswers.Add(questionData);
+                // db.SaveChanges();
+
+                return Ok("Question added successfully.");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
